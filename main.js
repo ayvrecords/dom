d512e84354d9a26b339c8336511b0df71041b09b
andrés felipe gonzález rojas <andresg.sax@hotmail.com>
2022-12-29 11:25:35 -0500

    cambios

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ primeraCaja.innerHTML = '<u>yurley</u>';
 	// 1.- Crear el elemento
 	var caja = document.createElement('div');
 	// 2.- Crear uno nodo de texto
-	var contenido = document.createTextNode('Hola Mundo');
+	var contenido = document.createTextNode(' Mundo');
 	// 3.- Añadir el nodo de texto al elemento
 	caja.appendChild(contenido);
 	// 4.- Agregar atributos a los caja
@@ -23,6 +23,18 @@ primeraCaja.innerHTML = '<u>yurley</u>';
 	// 5.- Agregar el elemento al documento
 	// var contenedor = document.getElementById('contenedor');
 	contenedor.appendChild(caja);
+
+	var caja2 = document.createElement('div');
+	// 2.- Crear uno nodo de texto
+	var contenido2 = document.createTextNode(' Mundo2');
+	// 3.- Añadir el nodo de texto al elemento
+	caja2.appendChild(contenido2);
+	// 4.- Agregar atributos a los caja
+	caja2.setAttribute('class', 'caja naranja');
+
+	// 5.- Agregar el elemento al documento
+	// var contenedor = document.getElementById('contenedor');
+	contenedor.appendChild(caja2);
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 
